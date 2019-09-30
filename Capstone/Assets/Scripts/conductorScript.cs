e0748a5360e55b1a62c1fa983b10d3eb1f80c87f
Valentine Morin <zm724@nyu.edu>
2019-09-30 12:56:03 -0400

    music tracking!!!!!!
    i can read

---
+++ Capstone/Assets/Scripts/conductorScript.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class conductorScript : MonoBehaviour
+{
+    public static conductorScript instance;
+
+    //beats per minute
+    public float sBPM;
+   
+    //number of seconds per beat!!
+    public float secondsPerBeat;
+
+    //track song pos in seconds
+    public float songPos;
+
+    //track song pos in beats
+    public float songPosinBeats;
+
+    //seconds passed since song began
+    public float dspSongTime;
+
+    //musique,,,,,,
+    public AudioSource music;
+
+    //NOTE-- bc wii shop theme has no beat offset, this is currently hidden
+    //enter offset manually & enter so beat tracking runs properly
+    //public float offset;
+
+    //all pos in beats of song
+    public float[] notes;
+
+    //next spawned note
+    int nextIndex = 0;
+
+    //number of notes shown in advance
+    float beatsAdvance = 2;
+
+    public GameObject notePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+
+        //calculate number of seconds per beat
+        secondsPerBeat = 60f / sBPM;
+
+        //record time @ start
+        dspSongTime = (float)AudioSettings.dspTime;
+
+        //music start!!
+        music.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //track seconds since song began
+        songPos = (float)(AudioSettings.dspTime - dspSongTime);
+
+        //track seconds since song began + OFFSET
+        //songPos = (float)(AudioSettings.dspTime - dspSongTime - offset);
+
+        //track beats
+        //note !! beat starts at 0, so beats of songs will be one beat behind where they ought to
+        songPosinBeats = songPos / secondsPerBeat;
+
+        //quiietLY CROSSES THIS OUT Bc......... am not making guitar hero,
+        ////time to spawn notes, baby
+        //if (nextIndex <notes.Length && notes[nextIndex] < songPosinBeats + beatsAdvance)
+        //{
+        //    Instantiate(notePrefab, new Vector3(transform.position.x, transform.position.y), Quaternion.identity);
+        //    nextIndex++;
+        //}
+    }
+}
---
+++ Capstone/Assets/Scripts/syncRotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class syncRotate : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    gameObject.transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(0, 360, conductorScript.instance.songPosinBeats));
+    
+    }
+}
