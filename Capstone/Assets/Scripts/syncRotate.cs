eb1104e46957e5364190ef0d95bd270716a9bd45
Valentine Morin <zm724@nyu.edu>
2019-09-30 13:58:19 -0400

    i t i s t i m e t o s p i n
    beats rotate now...................

--- Capstone/Assets/Scripts/syncRotate.cs
+++ Capstone/Assets/Scripts/syncRotate.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class syncRotate : MonoBehaviour
 
 {
+    public conductorScript script;
+
+    //tHTANK S YUO IAN
+    public float theta;
+    public float r = 1;
+    public float speed = .1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.transform.position = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-    
-    gameObject.transform.rotation = Quaternion.Euler(0, 0, Mathf.Lerp(0, 360, conductorScript.instance.songPosinBeats));
-    
+        float angle = ((2 * Mathf.PI) * ((script.songPosinBeats%3) / 3));
+        //float angle = 0;
+        this.transform.localPosition = PointOnCircle(angle, r);
+        Debug.Log(angle);
+    }
+
+    Vector3 PointOnCircle(float angle, float radius)
+    {
+        return new Vector3(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle), 0);
     }
 }
