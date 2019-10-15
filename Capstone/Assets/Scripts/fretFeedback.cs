ac43f2e28fb9ce49d56196af0a3a3d2e704f025b
Unknown <ianouellette47@gmail.com>
2019-10-15 14:16:30 -0400

    Rhythm Basic Functionality
    Updated the rhythm prototype to how it will work as our central mechanic. Now prompts a wasd and arrow key combination to press, and identifies if the player's response is correct. Also includes visual feedback for hits and misses.

---
+++ Capstone/Assets/Scripts/fretFeedback.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fretFeedback : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+
+    //A script for the fret, the circle that the notes line up with. Mostly for feedback.
+
+    float scaleMod = 1f; //Modifies the scale of the fret.
+
+    Color fretCol = Color.white; //Color of the fret
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = new Vector2(scaleMod, scaleMod); //Set the scale of the fret
+        if (scaleMod > 1.01f || scaleMod < 0.99f) //If the scaleMod isn't 1, return to 1
+        {
+            scaleMod = Mathf.Lerp(scaleMod, 1, .2f);
+        }
+        else //If the size is close to normal, reset the color
+        {
+            sr.color = Color.white;
+        }
+    }
+
+    public void fretHit(bool h) //Whenever the fret gets hit. Pass a bool for if the hit was accurate
+    {
+        if(h) //If it hits, set the color and increase the scale
+        {
+            sr.color = Color.blue; //May change this and the miss color once our palette is finalized.
+            scaleMod = 1.4f;
+        }
+        else //On a miss, set the color and decrease the scale
+        {
+            sr.color = Color.red;
+            scaleMod = 0.8f;
+        }
+    }
+}
--- Capstone/Assets/Scripts/syncRotate.cs
+++ Capstone/Assets/Scripts/syncRotate.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class syncRotate : MonoBehaviour
 
@@ -18,7 +19,29 @@ public class syncRotate : MonoBehaviour
 
     private int score;
 
-    public bool inZone = false;
+    public bool inZone = false; //Whether the note is in the zone
+
+    public fretFeedback fret; //The script of the fret, the circle that the player wants to line up notes with.
+
+    float cooldown = 0f; //The cooldown for inputting keys. Prevents the game from considering a hit to be wrong because the player doesn't release the keys frame-perfectly.
+    float cN = 0.2f; //The cooldown amount. Put here for ease of testing.
+
+    public Text pressedKeyText; //Debug, what keys are being pressed
+    public Text targetText; //Debug feature, what keys are correct
+    public Text debugTargetText; //Debug feature, what keys are correct in the format of pressedKeyText
+
+    string keys = ""; //What keys are being pressed
+    string wasdK = ""; //Which wasd key
+    string arrowK = ""; //Which arrow key
+    string target = "UR"; //What keys the game wants you to press. Currently randomly generated
+                          // Left is wasd, right is arrow keys
+                          // U : up, D : down, L : left, r : right
+                          // So the default here corresponds to w + right arrow
+
+    bool primed = false; //If one key is pressed. This way it only considers a keypress when you press the second key, in case you aren't pressing both on the same frame.
+    bool keyed = false; //If both keys are pressed.
+    float primeCool = 0f; //How long you can keep one key pressed for before it registers an incorrect hit.
+    float pcN = 0.1f; //Default value for pcN
 
     // Start is called before the first frame update
     void Start()
@@ -30,26 +53,161 @@ public class syncRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float angle = ((2 * Mathf.PI) * ((script.songPosinBeats%3) / 3));
+        float angle = ((2 * Mathf.PI) * ((script.songPosinBeats % 3) / 3));
         //float angle = 0;
         this.transform.localPosition = PointOnCircle(angle, r);
         //Debug.Log(angle);
 
-        //play g a e m
-        if (inZone == true)
+        //Code for pressing keys
+        if (Input.GetKeyDown(KeyCode.W)) //When a key is pressed
         {
-            if (Input.GetKey(KeyCode.Space))
-                {
-                score++;
-                Debug.Log(score);
-
-                inZone = false;
-                    //reference script and engagE THE SHAKE
-                    //camScript cs = cam.GetComponent<camScript>();
-
-                    //cs.Shake();
-                }
+            wasdK += "U"; //Set which one was pressed
+            if(!primed) //If this is the first key pressed, note that one key has been pressed
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else //If this is the second key pressed, then confirm that a key combination has been entered
+            {
+                keyed = true;
+            }
         }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            wasdK += "L";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            wasdK += "D";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            wasdK += "R";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            wasdK += "U";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            wasdK += "L";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            wasdK += "D";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            wasdK += "R";
+            if (!primed)
+            {
+                primed = true;
+                primeCool = pcN;
+            }
+            else
+            {
+                keyed = true;
+            }
+        }
+
+        //A key is pressed
+        if (keyed && cooldown <= 0)
+        {
+            keys = wasdK + arrowK;
+            pressedKeyText.text = keys;
+            if (inZone == true) //If the timing is correct
+            {
+                if (keys.Equals(target)) //If the note is correct, add score and trigger feedback
+                {
+                    score++;
+                    Debug.Log(score);
+
+                    inZone = false;
+                    fret.fretHit(true);
+                }
+                else //Otherwise trigger negative feedback
+                {
+                    fret.fretHit(false);
+                }
+            }
+            else //Timing incorrect, negative feedback
+            {
+                fret.fretHit(false);
+            }
+            cooldown = cN;
+            primed = false;
+            keyed = false;
+            wasdK = "";
+        }
+
+        cooldown -= Time.deltaTime;
+
+        if(primed) //If one note has been pressed, trigger a miss if no other note is pressed within 0.1 seconds
+        {
+            if(primeCool <= 0)
+            {
+                fret.fretHit(false);
+                primed = false;
+                wasdK = "";
+            }
+            primeCool -= Time.deltaTime;
+        }
+
     }
 
     Vector3 PointOnCircle(float angle, float radius)
@@ -72,6 +230,63 @@ public class syncRotate : MonoBehaviour
         if (other.gameObject.tag == "beat")
         {
             inZone = false;
+            setTarget(); //Find a new key to want to press
         }
     }
+
+    void setTarget() //Set a new target key combination
+    {
+        target = ""; //Reset the old combination
+        string uiT = "";
+        for (int y = 0; y < 2; y++) //Do this twice
+        {
+            int x = Random.Range(0, 4); //Randomly assign up, down, left, or right
+            if (x == 0)
+            {
+                target += "U";
+                uiT += "^ ";
+            }
+            else if (x == 1)
+            {
+                target += "L";
+                uiT += "< ";
+            }
+            else if (x == 2)
+            {
+                target += "D";
+                uiT += "v ";
+            }
+            else if (x == 3)
+            {
+                target += "R";
+                uiT += "> ";
+            }
+        }
+        targetText.text = uiT; //Set debug direction indicator
+        debugTargetText.text = target;
+    }
+        
+
+    //Older code, from a simpler time
+    //Keeping it in case we need it
+    /*if (inZone == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    score++;
+                    Debug.Log(score);
+
+                    inZone = false;
+                    fret.fretHit(true);
+                    cooldown = cN;
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    fret.fretHit(false);
+                    cooldown = cN;
+                }
+            }*/
 }
