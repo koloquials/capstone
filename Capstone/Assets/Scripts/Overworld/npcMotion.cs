24a9e4429cc5851b1cec61f9dccdd14f468ff54b
Unknown <ianouellette47@gmail.com>
2020-04-22 16:14:18 -0400

    Implementing Stage Directions
    Updated NPC motion script to accommodate stage directions, and implemented them in the demo yarn script. Currently there are still some missing portrait sprites, the beginning fade from black isn't in, and we don't have dialogue during puzzles and the rhythm game yet.

--- Capstone/Assets/Scripts/Overworld/npcMotion.cs
+++ Capstone/Assets/Scripts/Overworld/npcMotion.cs
@@ -6,26 +6,48 @@ namespace Yarn.Unity.Example
 {
     public class npcMotion : MonoBehaviour
     {
-        public GameObject[] waypoints; //Invisible gameobjects that the npc moves to. For consistency's sake, waypoint 0 should be their starting position.
+        public GameObject[] pointArray; //Invisible gameobjects that the npc moves to. For consistency's sake, waypoint 0 should be their starting position.
+        Dictionary<string, GameObject> waypoints; //New format. String corresponds to waypoint's name.
 
-        int point = 0; //Which waypoint the npc is at.
+        //int point = 0; //Which waypoint the npc is at.
+        string point;
 
         bool moving = false; //Whether the npc is moving to a waypoint.
+        int motionType = 0;
 
         NPC npc; //The NPC script for this npc
 
         float motionTimer = 0;
 
-        Vector2 startPos;
+        float pushtimer = 0;
+        float pushcool = 0;
+
+        Vector3 startPos;
 
         SpriteRenderer sr;
 
         public Sprite standingSprite;
         public Sprite motionSprite;
 
+        public Sprite[] spriteArray; //List and dictionary of all the sprites used. Dictionary enables easier lookup, while the public array of sprites is necessary to assign things to the dictionary in the inspector
+        public Dictionary<string, Sprite> spriteList;
+
         // Start is called before the first frame update
         void Start()
         {
+            spriteList = new Dictionary<string, Sprite>();
+            foreach(Sprite s in spriteArray)
+            {
+                spriteList.Add(s.name, s);
+            }
+
+            waypoints = new Dictionary<string, GameObject>();
+            foreach(GameObject g in pointArray)
+            {
+                waypoints.Add(g.name, g);
+
+            }
+
             sr = this.GetComponent<SpriteRenderer>();
             npc = this.GetComponent<NPC>();
         }
@@ -35,41 +57,78 @@ namespace Yarn.Unity.Example
         {
             if(moving)
             {
-                if(Vector2.Distance(transform.position, waypoints[point].transform.position) > 0.1)
+                if (motionType == 0) //Normal movement
                 {
-                    transform.position = new Vector2(Mathf.Lerp(startPos.x, waypoints[point].transform.position.x, motionTimer/5f), transform.position.y);
-                    motionTimer += Time.deltaTime;
-                }
-                else
-                {
-                    moving = false;
-                    npc.setMotion(false);
-                    if(standingSprite != null)
+                    if (Vector2.Distance(transform.position, waypoints[point].transform.position) > 0.1)
                     {
-                        sr.sprite = standingSprite;
+                        transform.position = new Vector3(Mathf.Lerp(startPos.x, waypoints[point].transform.position.x, motionTimer / 5f), transform.position.y, transform.position.z);
+                        motionTimer += Time.deltaTime;
+                    }
+                    else
+                    {
+                        moving = false;
+                        //npc.setMotion(false);
+                        if (standingSprite != null)
+                        {
+                            sr.sprite = standingSprite;
+                        }
+                    }
+                }
+                else if (motionType == 1) //Pushing movement
+                {
+                    if (Vector2.Distance(transform.position, waypoints[point].transform.position) > 0.1)
+                    {
+                        if (pushtimer < 1)
+                        {
+                            transform.position = new Vector2(Mathf.Lerp(startPos.x, waypoints[point].transform.position.x, motionTimer / 5f), transform.position.y);
+                            motionTimer += Time.deltaTime;
+                            pushtimer += Time.deltaTime;
+                        }
+                        else
+                        {
+                            pushcool += Time.deltaTime;
+                            if(pushcool > 0.5f)
+                            {
+                                pushtimer = 0;
+                                pushcool = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        moving = false;
+                        //npc.setMotion(false);
+                        if (standingSprite != null)
+                        {
+                            sr.sprite = standingSprite;
+                        }
                     }
                 }
             }
         }
 
         [YarnCommand("moveNPC")]
-        public void nextPoint()
+        public void nextPoint(string waypoint) //Set the waypoint that the npc is going to. Set the key/value combinations in the inspector.
         {
-            if (point < waypoints.Length - 1)
+            if (waypoints.ContainsKey(waypoint))
             {
                 if(motionSprite != null)
                 {
                     sr.sprite = motionSprite;
                 }
-                point += 1;
+                point = waypoint;
                 moving = true;
                 motionTimer = 0;
                 startPos = transform.position;
-                npc.setMotion(true);
+                if (startPos.x < waypoints[point].transform.position.x)
+                    sr.flipX = true;
+                else
+                    sr.flipX = false;
+                //npc.setMotion(true);
             }
             else
             {
-                Debug.Log("Not enough waypoints");
+                Debug.Log("Invalid waypoint");
             }
         }
 
@@ -77,6 +136,47 @@ namespace Yarn.Unity.Example
         public void appear() //Used for npcs or other overworld elements that are not present at start. These objects should have their sprite color set to transparent
         {
             sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 1);
+        }
+
+        [YarnCommand("changeNPC")]
+        public void spriteChange(string spriteName) //Sets the overworld sprite of the npc
+        {
+            if(spriteList.ContainsKey(spriteName))
+            {
+                if(!moving) //If the sprite isn't moving, set the new sprite right away
+                {
+                    sr.sprite = spriteList[spriteName];
+                }
+                standingSprite = spriteList[spriteName]; //In case the sprite is moving, set it so that it reverts to this sprite when done moving
+            }
+        }
+
+        [YarnCommand("setNPCMove")]
+        public void setMovement(string type) //Potentially sets npc movement to different types. Right now 0 is default, 1 is fidel pushing things
+        {
+            if (type.Equals("normal"))
+            {
+                motionType = 0;
+            }
+            else if (type.Equals("push"))
+            {
+                motionType = 1;
+            }
+        }
+
+        [YarnCommand("warpNPC")]
+        public void warp() //Instantly finishes npc movement. Used for dialogue skipping.
+        {
+            if(moving)
+            {
+                transform.position = waypoints[point].transform.position;
+                moving = false;
+                //npc.setMotion(false);
+                if (standingSprite != null)
+                {
+                    sr.sprite = standingSprite;
+                }
+            }
         }
     }
 }
