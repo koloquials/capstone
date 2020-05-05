796260d7b091bd1858ddeeac48a26c50c6938f94
Samantha Chuang <samantha.s.chuang@gmail.com>
2020-05-05 15:04:22 -0400

    Added a package for breaking objects, cleaned up main scene (reorganised GameObjects, grouped teleport objects, etc...)

--- Capstone/Assets/Scripts/ObjectProperties/MaterialManager.cs
+++ Capstone/Assets/Scripts/ObjectProperties/MaterialManager.cs
@@ -10,18 +10,40 @@ using UnityEngine;
 public class MaterialManager : MonoBehaviour
 {
     public Material[] materials;
+    public Material dissolveMat;
     public InteractableHighlight highlightScript;
     public Dissolve dissolveScript;
+
+    public Explodable explodeScript;
+
+    public Texture mainTex;
 
     // Start is called before the first frame update
     void Start() {
         highlightScript = gameObject.GetComponent<InteractableHighlight>();
         dissolveScript = gameObject.GetComponent<Dissolve>();
+        explodeScript = gameObject.GetComponent<Explodable>();
+
+
+        foreach (Transform shard in transform) {
+            MeshRenderer shardRenderer = shard.gameObject.GetComponent<MeshRenderer>();
+
+            // Destroy(shardRenderer);
+            shard.gameObject.AddComponent<Dissolve>();
+
+            Dissolve shardDissolve = shard.gameObject.GetComponent<Dissolve>();
+            shardDissolve.dissolveMat = this.dissolveMat;
+            shardDissolve.mainTex = this.mainTex;
+        }
     }
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.B)) {
             Dissolve();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T)) {
+            explodeScript.explode();
         }
     }
 
